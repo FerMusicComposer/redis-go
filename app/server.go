9b60757261fc341497e072683d4120a9b799498b
Fernando Salamanca <67641798+FerMusicComposer@users.noreply.github.com>
2025-02-01 13:43:02 +0100

    Implement continuous ping response handling

--- app/server.go
+++ app/server.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -15,12 +15,23 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	conn.Write([]byte("+PONG\r\n"))
+	reader := bufio.NewReader(conn)
+	for {
+		_, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			break
+		}
+
+		conn.Write([]byte("+PONG\r\n"))
+	}
+
 	conn.Close()
 }
