9bc101420a95736da3625db1b4c5cdc19cdb10f6
Fernando Salamanca <67641798+FerMusicComposer@users.noreply.github.com>
2025-02-01 15:13:54 +0100

    Implement RESP protocol parsing and ECHO command support

--- app/server.go
+++ app/server.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -31,25 +32,68 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-
 	reader := bufio.NewReader(conn)
+
 	for {
-		data, err := reader.ReadBytes('\n')
+		// Parse RESP array
+		line, _, err := reader.ReadLine()
 		if err != nil {
 			if err == io.EOF {
 				break
 			}
+
 			fmt.Println("Error reading from connection: ", err.Error())
 			break
 		}
 
-		if len(data) < 2 || data[len(data)-2] != '\r' {
+		// Check array length
+		if len(line) == 0 || line[0] != '*' {
 			continue
 		}
 
-		commandLine := string(data[:len(data)-2])
-		if strings.ToUpper(commandLine) == "PING" {
-			conn.Write([]byte("+PONG\r\n"))
+		arraySize, err := strconv.Atoi(string(line[1:]))
+		if err != nil || arraySize < 1 {
+			continue
 		}
+
+		var command string
+		var args []string
+
+		for i := 0; i < arraySize; i++ {
+			line, _, err = reader.ReadLine()
+			if err != nil || len(line) < 1 || line[0] != '$' {
+				break
+			}
+
+			strLength, err := strconv.Atoi(string(line[1:]))
+			if err != nil || strLength < 0 {
+				break
+			}
+
+			strBytes := make([]byte, strLength)
+			_, err = io.ReadFull(reader, strBytes)
+			if err != nil {
+				break
+			}
+
+			reader.Discard(2)
+
+			if i == 0 {
+				command = strings.ToUpper(string(strBytes))
+			} else {
+				args = append(args, string(strBytes))
+			}
+		}
+
+		switch command {
+		case "PING":
+			conn.Write([]byte("+PONG\r\n"))
+		case "ECHO":
+			if len(args) > 0 {
+				response := fmt.Sprintf("$%d\r\n%s\r\n", len(args[0]), args[0])
+				conn.Write([]byte(response))
+			}
+		}
+
 	}
 }
