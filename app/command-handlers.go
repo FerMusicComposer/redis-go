e5541569a9c4d9ea33e1fd9e6a6deb7466a14585
Fernando Salamanca <67641798+FerMusicComposer@users.noreply.github.com>
2025-02-01 19:32:59 +0100

    Refactor command handling with command registry and modular handlers

---
+++ app/command-handlers.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type CommandHandler func(args []string) string
+
+var registry = map[string]struct {
+	minArgs int
+	maxArgs int
+	handler CommandHandler
+}{
+	"SET":  {2, 4, setCommand},
+	"GET":  {1, 1, getCommand},
+	"PING": {0, 0, pingCommand},
+	"ECHO": {1, 1, echoCommand},
+}
+
+func handleCommand(command string, args []string) string {
+	cmd, exists := registry[strings.ToUpper(command)]
+	if !exists {
+		return fmt.Sprintf("-ERR unknown command '%s'\r\n", command)
+	}
+
+	if len(args) < cmd.minArgs || len(args) > cmd.maxArgs {
+		return fmt.Sprintf("-ERR wrong number of arguments for '%s' command\r\n", strings.ToUpper(command))
+	}
+
+	return cmd.handler(args)
+}
--- app/commands.go
+++ app/commands.go
@@ -10,65 +10,127 @@ type storedValue struct {
 	expiresAt time.Time
 }
 
-func handleCommand(command string, args []string) string {
-	switch command {
-	case "PING":
-		return "+PONG\r\n"
-	case "ECHO":
-		if len(args) == 0 {
-			return "-ERR wrong number of arguments for 'echo' command\r\n"
-		}
-
-		return fmt.Sprintf("$%d\r\n%s\r\n", len(args[0]), args[0])
-
-	case "SET":
-		if len(args) != 2 && len(args) != 4 {
-			return "-ERR wrong number of arguments for 'SET' command\r\n"
-		}
-
-		var expiresAt time.Time
-		var errStr string
-
-		if len(args) == 4 {
-			expiresAt, errStr = parseCommandExpiry(args)
-			if errStr != "" {
-				return errStr
-			}
-		}
-
-		storage.Store(args[0], &storedValue{
-			value:     args[1],
-			expiresAt: expiresAt,
-		})
-
-		return "+OK\r\n"
-
-	case "GET":
-		if len(args) != 1 {
-			return "-ERR wrong number of arguments for 'SET' command\r\n"
-		}
-
-		val, ok := storage.Load(args[0])
-		if !ok {
-			return "$-1\r\n"
-		}
-
-		// Type assertion and expiration check
-		sv, ok := val.(*storedValue)
-		if !ok {
-			storage.Delete(args[0]) // Clean up invalid data
-			return "$-1\r\n"
-		}
-
-		// Check expiration if set
-		if !sv.expiresAt.IsZero() && time.Now().After(sv.expiresAt) {
-			storage.Delete(args[0])
-			return "$-1\r\n" // Null bulk string for missing keys
-		}
-
-		return fmt.Sprintf("$%d\r\n%s\r\n", len(sv.value), sv.value)
-
-	default:
-		return "-ERR unknown command '" + command + "'\r\n"
-	}
+func pingCommand(args []string) string {
+	_ = args
+	return "+PONG\r\n"
 }
+
+func echoCommand(args []string) string {
+	if len(args) == 0 {
+		return "-ERR wrong number of arguments for 'echo' command\r\n"
+	}
+
+	return fmt.Sprintf("$%d\r\n%s\r\n", len(args[0]), args[0])
+}
+
+func setCommand(args []string) string {
+	if len(args) != 2 && len(args) != 4 {
+		return "-ERR wrong number of arguments for 'SET' command\r\n"
+	}
+
+	var expiresAt time.Time
+	var errStr string
+
+	if len(args) == 4 {
+		expiresAt, errStr = parseCommandExpiry(args)
+		if errStr != "" {
+			return errStr
+		}
+	}
+
+	storage.Store(args[0], &storedValue{
+		value:     args[1],
+		expiresAt: expiresAt,
+	})
+
+	return "+OK\r\n"
+}
+
+func getCommand(args []string) string {
+	if len(args) != 1 {
+		return "-ERR wrong number of arguments for 'SET' command\r\n"
+	}
+
+	val, ok := storage.Load(args[0])
+	if !ok {
+		return "$-1\r\n"
+	}
+
+	// Type assertion and expiration check
+	sv, ok := val.(*storedValue)
+	if !ok {
+		storage.Delete(args[0]) // Clean up invalid data
+		return "$-1\r\n"
+	}
+
+	// Check expiration if set
+	if !sv.expiresAt.IsZero() && time.Now().After(sv.expiresAt) {
+		storage.Delete(args[0])
+		return "$-1\r\n" // Null bulk string for missing keys
+	}
+
+	return fmt.Sprintf("$%d\r\n%s\r\n", len(sv.value), sv.value)
+}
+
+// func handleCommand(command string, args []string) string {
+// 	switch command {
+// 	case "PING":
+// 		return "+PONG\r\n"
+// 	case "ECHO":
+// 		if len(args) == 0 {
+// 			return "-ERR wrong number of arguments for 'echo' command\r\n"
+// 		}
+
+// 		return fmt.Sprintf("$%d\r\n%s\r\n", len(args[0]), args[0])
+
+// 	case "SET":
+// 		if len(args) != 2 && len(args) != 4 {
+// 			return "-ERR wrong number of arguments for 'SET' command\r\n"
+// 		}
+
+// 		var expiresAt time.Time
+// 		var errStr string
+
+// 		if len(args) == 4 {
+// 			expiresAt, errStr = parseCommandExpiry(args)
+// 			if errStr != "" {
+// 				return errStr
+// 			}
+// 		}
+
+// 		storage.Store(args[0], &storedValue{
+// 			value:     args[1],
+// 			expiresAt: expiresAt,
+// 		})
+
+// 		return "+OK\r\n"
+
+// 	case "GET":
+// 		if len(args) != 1 {
+// 			return "-ERR wrong number of arguments for 'SET' command\r\n"
+// 		}
+
+// 		val, ok := storage.Load(args[0])
+// 		if !ok {
+// 			return "$-1\r\n"
+// 		}
+
+// 		// Type assertion and expiration check
+// 		sv, ok := val.(*storedValue)
+// 		if !ok {
+// 			storage.Delete(args[0]) // Clean up invalid data
+// 			return "$-1\r\n"
+// 		}
+
+// 		// Check expiration if set
+// 		if !sv.expiresAt.IsZero() && time.Now().After(sv.expiresAt) {
+// 			storage.Delete(args[0])
+// 			return "$-1\r\n" // Null bulk string for missing keys
+// 		}
+
+// 		return fmt.Sprintf("$%d\r\n%s\r\n", len(sv.value), sv.value)
+
+// 	default:
+// 		return "-ERR unknown command '" + command + "'\r\n"
+// 	}
+// }
